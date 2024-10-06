b76d278c98f448e165b23b5105737b435be4cae3
哈哈哈哈哈18194085604 <484764968@qq.com>
2024-10-06 12:08:07 +0800

    feat:(初始化项目)

---
+++ src/test/java/com/gjy/openaisdkjava/OpenaiSdkJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gjy.openaisdkjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenaiSdkJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
